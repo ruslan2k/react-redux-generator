c52c5418f53a56b12fbd0e0b72bcaed3a642c386
Ruslan Karachun <free.changer@gmail.com>
2018-09-07 11:47:58 +0300

    Added store & router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+const Router = HashRouter
 
 function initApp() {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render((
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path='/' component={App} />
+        </Switch>
+      </Router>
+    </Provider>
+  ), document.getElementById('root'));
 }
 if (typeof cordova === 'object') {
   document.addEventListener('deviceready', () => {
